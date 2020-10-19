1f42016a94be01cc1da2fa57090ce1dcbbf26aa6
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-19 17:04:12 -0300

    initial definitions for graph crate

---
+++ graph/src/digraph.rs
@@ -0,0 +1,63 @@
+use matrix::Matrix;
+
+pub struct Digraph {
+    data: Matrix<bool>,
+
+    num_verts: usize,
+    num_edges: usize
+}
+
+// constructors
+impl Digraph {
+    pub fn new(num_verts: usize) -> Self{
+        let data = Matrix::square(num_verts);
+        let num_edges = 0;
+
+        Self {data, num_verts, num_edges}
+    }
+}
+
+// accessors
+impl Digraph {
+    pub fn num_verts(&self) -> usize {
+        self.num_verts
+    }
+
+    pub fn num_edges(&self) -> usize {
+        self.num_edges
+    }
+
+    pub fn has_edge(&self, i: usize, j: usize) -> bool {
+        *self.data.const_at(i, j)
+    }
+
+}
+
+type Result<'a> = std::result::Result<(), &'a str>;
+
+// modificators
+impl Digraph {
+    pub fn add_edge(&mut self, i: usize, j: usize) -> Result {
+        if self.has_edge(i, j) {
+            Err("Digraph: attempting to add existent edge")
+        }
+        else {
+            *self.data.at(i, j) = true;
+            self.num_edges += 1;
+
+            Ok(())
+        }
+    }
+
+    pub fn remove_edge(&mut self, i: usize, j: usize) -> Result {
+        if self.has_edge(i, j) {
+            *self.data.at(i, j) = false;
+            self.num_edges -= 1;
+
+            Ok(())
+        }
+        else {
+            Err("Digraph: attempting to remove nonexistent edge")
+        }
+    }
+}
---
+++ graph/src/lib.rs
@@ -0,0 +1,33 @@
+mod digraph;
+
+use crate::digraph::Digraph;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn digraph_instantiation() {
+        let mut d = Digraph::new(6);
+
+        assert_eq!(d.num_verts(), 6);
+        assert_eq!(d.num_edges(), 0);
+
+        assert_eq!(d.has_edge(3, 5), false);
+        d.add_edge(3, 5);
+        assert_eq!(d.has_edge(3, 5), true);
+        assert_eq!(d.num_edges(), 1);
+
+        assert_eq!(d.has_edge(5, 3), false);
+        d.add_edge(5, 3);
+        assert_eq!(d.has_edge(5, 3), true);
+        assert_eq!(d.num_edges(), 2);
+
+        d.add_edge(1, 2);
+        assert_eq!(d.num_edges(), 3);
+
+        d.remove_edge(5, 3);
+        assert_eq!(d.num_edges(), 2);
+        assert_eq!(d.has_edge(5, 3), false);
+    }
+}
