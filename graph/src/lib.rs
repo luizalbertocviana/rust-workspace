653fae02c372a8ca8f1df8ec40ee10d52df960e6
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-22 18:39:21 -0300

    added unwrap to the digraph modifiers calls

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -18,20 +18,22 @@ mod tests {
         assert_eq!(d.num_edges(), 0);
 
         assert_eq!(d.has_edge(3, 5), false);
-        d.add_edge(3, 5);
+        d.add_edge(3, 5).unwrap();
         assert_eq!(d.has_edge(3, 5), true);
         assert_eq!(d.num_edges(), 1);
 
         assert_eq!(d.has_edge(5, 3), false);
-        d.add_edge(5, 3);
+        d.add_edge(5, 3).unwrap();
         assert_eq!(d.has_edge(5, 3), true);
         assert_eq!(d.num_edges(), 2);
 
-        d.add_edge(1, 2);
+        d.add_edge(1, 2).unwrap();
         assert_eq!(d.num_edges(), 3);
 
-        d.remove_edge(5, 3);
+        d.remove_edge(5, 3).unwrap();
         assert_eq!(d.num_edges(), 2);
         assert_eq!(d.has_edge(5, 3), false);
+        assert!(d.remove_edge(5, 3).is_err());
+    }
     }
 }
