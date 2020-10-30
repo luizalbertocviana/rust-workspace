5e6a807a1590dfce42c85c20b826cf6f30c9188d
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-30 19:19:26 -0300

    major changed in solving_status.rs

--- branch-bound/src/solving_status.rs
+++ branch-bound/src/solving_status.rs
@@ -1,54 +1,83 @@
 use crate::Result;
-use crate::traits::{BBProblem, Solution, SolutionCost};
+use crate::traits::{BBProblem, Solution};
 
-type SolCost<T> = <<T as BBProblem>::Sol as Solution>::SolCost;
+type Sol<T> = <T as BBProblem>::Sol;
+type SolCost<T> = <Sol<T> as Solution>::SolCost;
 
 pub struct SolvingStatus<T: BBProblem> {
-    lower_bound: SolCost<T>,
-    upper_bound: SolCost<T>,
+    lower_bound: Option<SolCost<T>>,
 
-    valid_upper_bound: bool
+    best_solution: Option<Sol<T>>
 }
 
 // constructors
 impl<T: BBProblem> SolvingStatus<T> {
     pub fn new() -> Self {
-        let lower_bound = SolCost::<T>::min_value();
-        let upper_bound = SolCost::<T>::max_value();
-        let valid_upper_bound = false;
+        let lower_bound   = None;
+        let best_solution = None;
 
-        Self {lower_bound, upper_bound, valid_upper_bound}
+        Self {lower_bound, best_solution}
     }
 }
 
 // accessors
 impl<T: BBProblem> SolvingStatus<T> {
     pub fn finished(&self) -> bool {
-        self.valid_upper_bound && self.lower_bound == self.upper_bound
+        match (&self.lower_bound, &self.best_solution) {
+            (None, _) => false,
+            (_, None) => false,
+            (Some(lb), Some(sol)) => *lb == sol.get_cost()
+        }
+    }
+
+    pub fn lower_bound(&self) -> &Option<SolCost<T>> {
+        &self.lower_bound
+    }
+
+    pub fn best_solution(&self) -> &Option<Sol<T>> {
+        &self.best_solution
     }
 }
 
 // modifiers
 impl<T: BBProblem> SolvingStatus<T> {
     pub fn set_lower_bound(&mut self, lb: SolCost<T>) -> Result {
-        if lb <= self.upper_bound {
-            self.lower_bound = lb;
+        if let Some(sol) = &self.best_solution {
+            if lb > sol.get_cost() {
+                Err("SolvingStatus: attempt to set a lower bound greater than current upper bound")
+            }
+            else {
+                self.lower_bound.replace(lb);
 
-            Ok(())
+                Ok(())
+            }
         }
         else {
-            Err("SolvingStatus: attempt to set a lower bound greater than current upper bound")
+            self.lower_bound.replace(lb);
+
+            Ok(())
         }
     }
 
-    pub fn set_upper_bound(&mut self, ub: SolCost<T>) -> Result {
-        if ub >= self.lower_bound {
-            self.upper_bound = ub;
+    pub fn set_best_solution(&mut self, sol: Sol<T>) -> Result {
+        if let Some(best_sol) = &self.best_solution {
+            if best_sol.get_cost() > sol.get_cost() {
+                self.best_solution.replace(sol);
+
+                Ok(())
+            }
+            else {
+                Err("SolvingStatus: attempt to set a solution worse than current best solution")
+            }
+        }
+        else {
+            self.best_solution.replace(sol);
 
             Ok(())
         }
-        else {
-            Err("SolvingStatus: attempt to set an upper bound less than current lower bound")
-        }
+    }
+
+    pub fn extract_best_solution(self) -> Option<Sol<T>> {
+        self.best_solution
     }
 }
