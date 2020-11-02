f9ac10c3bc294c5a0a11eadb96497e29207cfc3f
Luiz Alberto <luizalbertocviana@gmail.com>
2020-11-02 18:34:46 -0300

    added new trait EdgeIterable in hope of some reuse

--- graph/src/traits.rs
+++ graph/src/traits.rs
@@ -8,3 +8,33 @@ pub trait GraphImpl<'a> {
     fn has_edge(&self, u: usize, v: usize) -> bool;
     fn edges(&'a self) -> Self::EdgeIterator;
 }
+
+pub trait EdgeIterable<'a> {
+    type Parent: GraphImpl<'a>;
+
+    fn parent(&self) -> &Self::Parent;
+    fn current_i(&self) -> usize;
+    fn current_j(&self) -> usize;
+
+    fn next_pair(&mut self);
+
+    fn stop(&self) -> bool {
+        self.current_j() == 0 && self.current_i() == self.parent().num_verts()
+    }
+
+    fn next(&mut self) -> Option<Edge> {
+        if self.stop() {
+            None
+        } else {
+            let pair = (self.current_i(), self.current_j());
+
+            self.next_pair();
+
+            if self.parent().has_edge(pair.0, pair.1) {
+                Some(pair)
+            } else {
+                self.next()
+            }
+        }
+    }
+}
