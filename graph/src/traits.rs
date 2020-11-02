5dcadd4c09fcadb9717322fac3364deebed4b8ef
Luiz Alberto <luizalbertocviana@gmail.com>
2020-11-02 18:33:45 -0300

    added some functions to GraphImpl

--- graph/src/traits.rs
+++ graph/src/traits.rs
@@ -4,5 +4,7 @@ pub trait GraphImpl<'a> {
     type EdgeIterator: Iterator<Item = Edge>;
 
     fn num_verts(&self) -> usize;
+    fn num_edges(&self) -> usize;
+    fn has_edge(&self, u: usize, v: usize) -> bool;
     fn edges(&'a self) -> Self::EdgeIterator;
 }
