7b7336df55b96d4ae7799b009f18435a9cda2a6d
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-04-18 21:01:30 +0000

    using %g to corretly parse instance filenames

--- instances/instance_ids_1.py
+++ instances/instance_ids_1.py
@@ -4,7 +4,7 @@ from typing import List
 # generator11 generator12 generator13 generator14 generator15
 
 def get_instance_ids() -> List[str]:
-    return ["%d_%.2f_%d-%d" % (n, d, b, i)
+    return ["%d_%g_%d-%d" % (n, d, b, i)
             for n in [30, 60, 90, 120, 150]
             for d in [0.25, 0.50, 0.75]
             for b in [2, 4, 8]
--- instances/instance_ids_2.py
+++ instances/instance_ids_2.py
@@ -4,7 +4,7 @@ from typing import List
 # generator16 generator17 generator18 generator19 generator20
 
 def get_instance_ids() -> List[str]:
-    return ["%d_%.2f_%.2f-%d" % (n, d, dag_d, i)
+    return ["%d_%g_%g-%d" % (n, d, dag_d, i)
             for n in [30, 60, 90, 120]
             for d in [0.25, 0.50, 0.75]
             for dag_d in [0.25, 0.50, 0.75]
