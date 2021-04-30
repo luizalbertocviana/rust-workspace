edfbfa5d44a58d1b280867f5b3114653b18aa04e
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-04-30 17:51:28 -0300

    generalized subgraph to accept any GraphImpl as parent

--- graph/src/subgraph.rs
+++ graph/src/subgraph.rs
@@ -7,25 +7,25 @@ use std::iter::{Chain, Copied};
 use crate::traits::{EdgeIterable, GraphImpl};
 // utility function for adjusting endpoints of an edge
 use crate::utils::adjust_endpoints;
-// currently we implement Subgraph as subgraphs of Graph type. We see
-// this can be further generalized
-use crate::graph::Graph;
 // some type aliases we use in these implementations
 use crate::{Edge, Result};
 /// represents a subgraph of an undirected graph
-pub struct Subgraph<'a> {
+pub struct Subgraph<'a, G: GraphImpl<'a>> {
     // supergraph reference
-    parent: &'a Graph,
+    parent: &'a G,
     // edges (either included or removed) that distinguish this
     // subgraph from its supergraph
     included_edges: HashSet<Edge>,
     removed_edges: HashSet<Edge>,
 }
-// constructors
-impl<'a> Subgraph<'a> {
+// constructors and parent reference
+impl<'a, G> Subgraph<'a, G>
+where
+    G: GraphImpl<'a>,
+{
     /// returns a subgraph of parent with the same vertex and edge
     /// sets
-    pub fn from_graph(parent: &'a Graph) -> Self {
+    pub fn from_graph(parent: &'a G) -> Self {
         let included_edges = HashSet::new();
         let removed_edges = HashSet::new();
 
@@ -48,10 +48,17 @@ impl<'a> Subgraph<'a> {
             removed_edges,
         }
     }
+    // returns a reference to its parent
+    pub fn parent(&self ) -> &G {
+        &self.parent
+    }
 }
 // GraphImpl implementation
-impl<'a> GraphImpl<'a> for Subgraph<'a> {
-    type EdgeIterator = EdgeIterator<'a>;
+impl<'a, G> GraphImpl<'a> for Subgraph<'a, G>
+where
+    G: GraphImpl<'a>,
+{
+    type EdgeIterator = EdgeIterator<'a, G>;
 
     fn num_verts(&self) -> usize {
         self.parent.num_verts()
@@ -78,8 +85,8 @@ impl<'a> GraphImpl<'a> for Subgraph<'a> {
         }
     }
 
-    fn edges(&self) -> EdgeIterator {
-        EdgeIterator::new(self)
+    fn edges(&'a self) -> EdgeIterator<'a, G> {
+        EdgeIterator::new(&self)
     }
 
     fn add_edge(&mut self, i: usize, j: usize) -> Result {
@@ -135,33 +142,47 @@ impl<'a> GraphImpl<'a> for Subgraph<'a> {
     }
 }
 /// controls iteration through the edges of a Subgraph
-pub struct EdgeIterator<'a> {
+pub struct EdgeIterator<'a, G: GraphImpl<'a>> {
     // reference to the Subgraph whose edges are being iterated through
-    parent: &'a Subgraph<'a>,
+    parent: &'a Subgraph<'a, G>,
 
     current_pair: (usize, usize),
     // iterator that combines iterators of the included edges HashSet
     // and the Subgraph
-    edge_it: Chain<Copied<hash_set::Iter<'a, Edge>>, crate::graph::EdgeIterator<'a>>,
+    edge_it: Chain<Copied<hash_set::Iter<'a, Edge>>, <G as GraphImpl<'a>>::EdgeIterator>,
 }
 // constructors
-impl<'a> EdgeIterator<'a> {
+impl<'a, G> EdgeIterator<'a, G>
+where
+    G: GraphImpl<'a>,
+{
     // returns a new EdgeIterator
-    fn new(parent: &'a Subgraph) -> Self {
-        let mut edge_it = parent.included_edges.iter().copied().chain(parent.parent.edges());
+    fn new(parent: &'a Subgraph<'a, G>) -> Self {
+        let mut edge_it = parent
+            .included_edges
+            .iter()
+            .copied()
+            .chain(parent.parent.edges());
         let current_pair = match edge_it.next() {
             Some(pair) => pair,
             None => (parent.num_verts(), 0),
         };
 
-        Self {parent, current_pair, edge_it}
+        Self {
+            parent,
+            current_pair,
+            edge_it,
+        }
     }
 }
 // EdgeIterable implementation
-impl<'a> EdgeIterable<'a> for EdgeIterator<'a> {
-    type Parent = Subgraph<'a>;
+impl<'a, G> EdgeIterable<'a> for EdgeIterator<'a, G>
+where
+    G: GraphImpl<'a>,
+{
+    type Parent = Subgraph<'a, G>;
 
-    fn parent(&self) -> &Subgraph<'a> {
+    fn parent(&self) -> &Subgraph<'a, G> {
         self.parent
     }
 
@@ -182,7 +203,10 @@ impl<'a> EdgeIterable<'a> for EdgeIterator<'a> {
     }
 }
 // Iterator implementation for EdgeIterator
-impl<'a> Iterator for EdgeIterator<'a> {
+impl<'a, G> Iterator for EdgeIterator<'a, G>
+where
+    G: GraphImpl<'a>,
+{
     type Item = Edge;
 
     fn next(&mut self) -> Option<Edge> {
