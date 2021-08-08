5f4f70845ccfb5e165599c48cd48b6db8cb34d05
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-08-08 17:31:43 -0300

    added a partial implementation of branch_bound traits

--- instance-bb/src/lib.rs
+++ instance-bb/src/lib.rs
@@ -19,7 +19,74 @@ struct EdgeWeight {
 
 struct Solution<'a> {
     edges: HashSet<Edge>,
-    subgraph: Subgraph<'a, WGraph>
+    subgraph: Subgraph<'a, WGraph>,
+    parent_problem: &'a Problem<'a>,
+}
+
+impl<'a> Solution<'a> {
+    fn satisfies_dependencies(&self) -> bool {
+        self.edges.iter().all(|edge| {
+            let num_deps = num_deps(self, edge);
+            let edge_tuple = (edge.u, edge.v);
+            let lb = *self
+                .parent_problem
+                .instance
+                .dep_lb()
+                .get(&edge_tuple)
+                .unwrap();
+            let ub = *self
+                .parent_problem
+                .instance
+                .dep_ub()
+                .get(&edge_tuple)
+                .unwrap();
+
+            lb <= num_deps && num_deps <= ub
+        })
+    }
+}
+
+fn num_deps(sol: &Solution, edge: &Edge) -> usize {
+    let e_idx = sol
+        .parent_problem
+        .edge_to_index
+        .get(edge)
+        .expect("instance does not contain such an edge");
+
+    neighbors::in_neighbors(sol.parent_problem.instance.dependencies(), *e_idx)
+        .filter(|dep_idx| {
+            sol.edges
+                .contains(&sol.parent_problem.index_to_edge[*dep_idx])
+        })
+        .count()
+}
+
+struct Problem<'a> {
+    instance: &'a Instance,
+    edge_to_index: HashMap<Edge, usize>,
+    index_to_edge: Vec<Edge>,
+}
+
+impl<'a> Problem<'a> {
+    fn new(instance: &'a Instance) -> Self {
+        let mut edge_to_index = HashMap::new();
+        let mut index_to_edge = Vec::new();
+
+        let mut i = 0;
+
+        for (u, v) in instance.graph().edges() {
+            index_to_edge.push(Edge { u, v });
+            edge_to_index.insert(Edge { u, v }, i);
+
+            i += 1;
+        }
+
+        Problem {
+            instance,
+            edge_to_index,
+            index_to_edge,
+        }
+    }
 }
 
 impl Variable for Edge {
@@ -33,21 +100,36 @@ impl<'a> bb::Solution for Solution<'a> {
     type SolCost = EdgeWeight;
 
     fn is_feasible(&self) -> bool {
-        properties::is_spanning_tree(&self.subgraph)
+        properties::is_spanning_tree(&self.subgraph) && self.satisfies_dependencies()
     }
 
     fn get_cost(&self) -> EdgeWeight {
         let wg = self.subgraph.parent();
 
-        let mut cost = 0;
-        for edge in &self.edges {
-            cost += wg.get_edge_weight(edge.u, edge.v).unwrap();
+        EdgeWeight {
+            weight: self
+                .edges
+                .iter()
+                .map(|edge| wg.get_edge_weight(edge.u, edge.v).unwrap())
+                .sum(),
         }
-
-        EdgeWeight { weight: cost }
     }
 
     fn get_value(&self, var: &Edge) -> bool {
         self.edges.contains(var)
+    }
+}
+
+impl<'a> BBProblem for Problem<'a> {
+    type Sol = Solution<'a>;
+
+    type SubproblemIterator;
+
+    fn solve_relaxation(&self) -> Self::Sol {
+        todo!()
+    }
+
+    fn get_subproblems(&self) -> Self::SubproblemIterator {
+        todo!()
     }
 }
