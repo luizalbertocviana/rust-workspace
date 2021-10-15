bde6f99afbfe01666459c2169cbbebd880543059
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-10-15 14:02:20 -0300

    moves some traits from lib.rs to traits.rs

---
+++ solving-manager/src/traits.rs
@@ -0,0 +1,36 @@
+use std::{fmt::Display, time::{Duration, Instant}};
+
+pub trait Header {
+    fn header() -> String;
+}
+
+pub trait Solve {
+    type SolvingInfo: Display + Header;
+
+    fn solve(&self) -> Self::SolvingInfo;
+}
+
+pub type SolvingInfo<S> = <S as Solve>::SolvingInfo;
+
+pub type BenchmarkInfo<S> = (Duration, SolvingInfo<S>);
+
+pub trait Benchmark: Solve {
+    fn benchmark(&self) -> BenchmarkInfo<Self> {
+        let starting_instant = Instant::now();
+
+        let info = self.solve();
+
+        let ending_instant = Instant::now();
+
+        (ending_instant.duration_since(starting_instant), info)
+    }
+}
+
+pub type ReadingInfo<R> = <R as ReadBenchmark>::ReadingInfo;
+
+pub trait ReadBenchmark {
+    type Benchmark: Benchmark;
+    type ReadingInfo;
+    
+    fn read_benchmark(reading_info: Self::ReadingInfo) -> Self::Benchmark;
+}
--- solving-manager/src/utils.rs
+++ solving-manager/src/utils.rs
@@ -1,11 +1,11 @@
-use std::{fs::File, io::Write, rc::Rc};
+pub use std::{fs::File, io::Write};
 
-use crate::{Benchmark, BenchmarkInfo, Header};
+use crate::traits::{Benchmark, BenchmarkInfo, Header, ReadBenchmark, ReadingInfo};
 
 fn multiple_benchmark<B, IB>(benchs: IB) -> impl Iterator<Item = BenchmarkInfo<B>>
 where
     B: Benchmark,
-    IB: Iterator<Item = Rc<B>>,
+    IB: Iterator<Item = B>,
 {
     benchs.map(|bench| bench.benchmark())
 }
@@ -13,12 +13,12 @@ where
 pub fn write_multiple_benchmark<B, IB>(benchs: IB, filename: &String)
 where
     B: Benchmark,
-    IB: Iterator<Item = Rc<B>>,
+    IB: Iterator<Item = B>,
 {
     let mut file =
         File::create(filename).expect(&format!("error while creating file {}", filename));
 
-    let augmented_header = format!("Time {}", B::SolvingInfo::header());
+    let augmented_header = format!("Time {}\n", B::SolvingInfo::header());
 
     file.write_all(augmented_header.as_bytes()).expect(&format!(
         "error while writing the header of file {}",
@@ -26,11 +26,21 @@ where
     ));
 
     for (duration, info) in multiple_benchmark(benchs) {
-        let bench_str = format!("{} {}", duration.as_secs_f64(), info);
+        let bench_str = format!("{} {}\n", duration.as_secs_f64(), info);
 
         file.write_all(bench_str.as_bytes()).expect(&format!(
             "error while writing benchmark line in file {}",
             filename
         ));
     }
+}
+
+pub fn perform_multiple_benchmarks<R, IR>(reading_infos: IR, filename: &String)
+where
+    R: ReadBenchmark,
+    IR: Iterator<Item = ReadingInfo<R>>,
+{
+    let benchs = reading_infos.map(|reading_info| R::read_benchmark(reading_info));
+
+    write_multiple_benchmark(benchs, filename)
 }
