5028d4ed51a42eff3a201d58c8c6a1dde2728358
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-10-20 21:51:55 -0300

    first version of solve.rs

---
+++ instance-bb-solver/src/bin/solver.rs
@@ -0,0 +1,57 @@
+use std::fs::{read_dir, DirEntry};
+
+use instance_bb_solver::{ReadBenchmarkImpl, ReadingInfo};
+use solving_manager::utils::perform_multiple_benchmarks;
+
+fn main() {
+    let num_workers = 4;
+
+    let output_file = "parallel_branch_bound".to_string();
+
+    let files: Vec<DirEntry> = read_dir("./")
+        .expect("error while reading current directory contents")
+        .map(|entry| entry.expect("error while reading a particular entry in current directory"))
+        .filter(|entry| {
+            entry
+                .metadata()
+                .expect("error while getting metadata of a particular entry in current directory")
+                .is_file()
+        })
+        .collect();
+
+    let g_files: Vec<&DirEntry> = files
+        .iter()
+        .filter(|file| {
+            file.file_name()
+                .to_str()
+                .expect("error while getting name of a particular file in current directory")
+                .starts_with("G_")
+        })
+        .collect();
+
+    let reading_infos: Vec<ReadingInfo> = g_files
+        .iter()
+        .map(|graph_file| -> ReadingInfo {
+            let common_suffix: String = graph_file
+                .file_name()
+                .to_str()
+                .unwrap()
+                .chars()
+                .skip(2)
+                .collect();
+
+            let graph_file = "G_".to_string() + &common_suffix;
+            let deps_file = "D_".to_string() + &common_suffix;
+            let bounds_file = "B_".to_string() + &common_suffix;
+
+            ReadingInfo {
+                graph_file,
+                deps_file,
+                bounds_file,
+                description: common_suffix,
+                num_workers,
+            }
+        }).collect();
+
+    perform_multiple_benchmarks::<ReadBenchmarkImpl, _>(reading_infos.into_iter(), &output_file);
+}
